8eb04cafba5db8addd5ad70c50f0dc8dc32aed43
DeepthiUma <k.deepthiuma@gmail.com>
2021-02-18 22:43:51 +0530

    Update BinaryTree.cxx
    FIND: 1
    Traversal - PreOrder: 7 4 1 6 9 8 10 
    Traversal - InOrder: 1 4 6 7 8 9 10 
    Traversal - PostOrder: 1 6 4 8 10 9 7 
    Min: 1
    compare two tree: 1
    isBinarySearchTree: 1
    isBinarySearchTree: 0

--- BinaryTree.cxx
+++ BinaryTree.cxx
@@ -3,11 +3,12 @@
 #include<stdlib.h>
 #include<string.h>
 #include<cstdio>
+#include <math.h>
 using namespace std;
 
 class BinaryTree
 {
-private:
+public:
     class Node
     {
         public:
@@ -21,6 +22,7 @@ private:
                 rightChild = NULL;
             }
     };
+
 Node *root;
 public:
 BinaryTree()
@@ -35,12 +37,13 @@ Node* create(int N)
 }
 void insert(int value)
 {
-    Node *current = NULL;
-    Node *tmp = create(value);
     if(root == NULL)
-        root = tmp;
+    {
+        root = create(value);
+        return;
+    }
     
-    current = root;
+    Node *current = root;
     while(1)
     {
         if(current->item >value)
@@ -77,11 +80,100 @@ bool find(int value)
     }
     return false;
 }
-void print()
+void traversePreOrder()
 {
+    pTraversePreOrder(root);
+}
+void traverseInOrder()
+{
+    pTraverseInOrder(root);
+}
+void traversePostOrder()
+{
+    pTraversePostOrder(root);
+}
+private:
+int pTraversePreOrder(Node* root)
+{
+    if(root == NULL) return 0;
+    cout << root->item << " ";
+    pTraversePreOrder(root->leftChild);
+    pTraversePreOrder(root->rightChild);
+}
+void pTraverseInOrder(Node* root)
+{
+    if(root == NULL) return;
+    pTraverseInOrder(root->leftChild);
+    cout << root->item << " ";
+    pTraverseInOrder(root->rightChild);
+}
+void pTraversePostOrder(Node* root)
+{
+    if(root == NULL) return;
+    pTraversePostOrder(root->leftChild);
+    pTraversePostOrder(root->rightChild);
+    cout << root->item << " ";
+}
+public:
+int minValueInTree()
+{
+    return minValueInTree(root);
+}
+private:
+int min(int l,int r)
+{
+    if(l<r) return l;
+    else return r;
     
 }
+int minValueInTree(Node* root)
+{
+    if (root == NULL) return -1;
+    if(root->leftChild == NULL && root->rightChild == NULL) return root->item;
+    int left = minValueInTree(root->leftChild);
+    int right = minValueInTree(root->leftChild);
+    return min(left,right);
+}
 
+private:
+bool equals(Node* first, Node* second)
+{
+    if((first == NULL) && (second ==NULL)) return true;
+    
+    if((first!=NULL) && (second!=NULL))
+    {
+        return first->item == second->item 
+            && equals(first->leftChild,second->leftChild)
+            && equals(first->rightChild,second->rightChild);
+    }
+}
+public:
+bool equals(BinaryTree* otherTree)
+{
+    return equals(root,otherTree->root);
+}
+private:
+bool isBinarySearchTree(Node* root, int min,int max)
+{
+    if(root == NULL) return true;
+    
+    if((root->item < min) || (root->item > max)) return false;
+    
+    return (isBinarySearchTree(root->leftChild,min,root->item-1) && 
+            isBinarySearchTree(root->rightChild,root->item+1,max));
+    
+}
+public:
+bool isBinarySearchTree()
+{
+    return isBinarySearchTree(root, -100, 100);
+}
+void swapRoot()
+{
+    Node *tmp = root->leftChild;
+    root->rightChild = root->leftChild;
+    root->leftChild = tmp;
+}
 };
 int main()
 {
@@ -89,10 +181,33 @@ int main()
     tree->insert(7);
     tree->insert(4);
     tree->insert(9);
-    tree->insert(6);
     tree->insert(1);
-    tree->insert(11);
+    tree->insert(6);
+    tree->insert(8);
+    tree->insert(10);
     cout << "FIND: " << tree->find(9) << endl;
-    
+    cout << "Traversal - PreOrder: ";
+    tree->traversePreOrder();
+    cout << endl;
+    cout << "Traversal - InOrder: ";
+    tree->traverseInOrder();
+    cout << endl;
+    cout << "Traversal - PostOrder: " ;
+    tree->traversePostOrder();
+    cout << endl;
+    cout << "Min: " << tree->minValueInTree();
+    BinaryTree *tree2 = new BinaryTree();
+    tree2->insert(7);
+    tree2->insert(4);
+    tree2->insert(9);
+    tree2->insert(1);
+    tree2->insert(6);
+    tree2->insert(8);
+    tree2->insert(10);
+    cout << endl;
+    cout << "compare two tree: " << tree->equals(tree2) << endl;
+    cout << "isBinarySearchTree: " << tree->isBinarySearchTree() << endl;
+    tree->swapRoot();
+    cout << "isBinarySearchTree: " << tree->isBinarySearchTree() << endl;
     return 0;
 }
